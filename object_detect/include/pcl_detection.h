ccabb5542ee7113d7dca2921762383dc5468896c
menting <menting@tudelft.nl>
2022-05-21 15:09:48 +0200

    temp

--- object_detect/include/pcl_detection.h
+++ object_detect/include/pcl_detection.h
@@ -1,7 +1,15 @@
+#include <ros/ros.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <ros/callback_queue.h>
+
 #include <pcl/pcl_base.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
@@ -22,7 +30,7 @@ protected:
     
     void PCLfilter();
 
-    void detection();
+    // void detection();
 
     // Output container
     sensor_msgs::PointCloud2 output;
@@ -35,3 +43,47 @@ protected:
     ros::Publisher _detectionPub;
 };
 
+PCLdetect::PCLdetect(ros::NodeHandle& nh,
+                     ros::NodeHandle& pnh,
+                     const std::string &topic):
+  _nh(nh),
+  _pnh(pnh)
+{
+    ros::Subscriber sub = _nh.subscribe(topic, 1, &PCLdetect::PCLcallback, this);
+}
+
+PCLdetect::~PCLdetect()
+{
+    
+}
+
+
+void PCLdetect::PCLcallback(const sensor_msgs::PointCloud2ConstPtr& msg)
+{
+    // _pclTimeStamp = msg->header;
+    pcl_conversions::toPCL(*msg, cloud);
+
+    PCLfilter();
+
+}
+
+void PCLdetect::PCLfilter()
+{
+    // Create object and pointer for SOR
+    pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud(new pcl::PointCloud< pcl::PointXYZ>);
+ 
+    pcl::fromPCLPointCloud2( cloud, *point_cloud);
+    
+    // Perform filtering
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+    sor.setInputCloud (point_cloud);
+    sor.setMeanK (50);
+    sor.setStddevMulThresh (1.0);
+    sor.filter (cloud_filtered);
+
+}
+
+// void PCLdetect::detection()
+// {
+//     // TOOD
+// }
--- object_detect/include/person_detect.h
+++ object_detect/include/person_detect.h
@@ -1,22 +1,23 @@
-// // ROS headers
-// #include <ros/ros.h>
-// #include <cv_bridge/cv_bridge.h>
-// #include <sensor_msgs/image_encodings.h>
-// #include <ros/callback_queue.h>
-// #include <sensor_msgs/Image.h>
+// ROS headers
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <ros/callback_queue.h>
+#include <sensor_msgs/Image.h>
 #include <image_transport/image_transport.h>
-// #include <object_recognition_msgs/RecognizedObject.h>
+#include <object_recognition_msgs/RecognizedObject.h>
 
+// OpenCV headers
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-// // Boost headers
-// #include <boost/scoped_ptr.hpp>
-// #include <boost/foreach.hpp>
+// Boost headers
+#include <boost/scoped_ptr.hpp>
+#include <boost/foreach.hpp>
 
-// // Std C++ headers
-// #include <vector>
+// Std C++ headers
+#include <vector>
 
 class PersonDetector
 {
@@ -53,3 +54,115 @@ protected:
   ros::Publisher _detectionPub;
 
 };
+
+
+PersonDetector::PersonDetector(ros::NodeHandle& nh,
+                               ros::NodeHandle& pnh,
+                               double imageScaling, 
+                               const std::string &topic, 
+                               const std::string &transport):
+  _nh(nh),
+  _pnh(pnh),
+  _imageScaling(imageScaling),
+  _imageTransport(nh),
+  _privateImageTransport(pnh)
+{  
+
+  _hogCPU.reset( new cv::HOGDescriptor );
+  _hogCPU->setSVMDetector( cv::HOGDescriptor::getDefaultPeopleDetector() );
+
+  image_transport::TransportHints transportHint(transport);
+
+  _imageSub   = _imageTransport.subscribe(topic, 1, &PersonDetector::imageCallback, this, transportHint);
+
+  _detectionPub = _pnh.advertise<object_recognition_msgs::RecognizedObject>("results", 1);
+
+  cv::namedWindow("person detections");
+}
+
+PersonDetector::~PersonDetector()
+{
+  cv::destroyWindow("person detections");
+}
+
+void PersonDetector::imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  // Image handling from ROS msg to OpenCV format
+  cv_bridge::CvImageConstPtr cvImgPtr;
+  cvImgPtr = cv_bridge::toCvShare(msg);
+
+  _imgTimeStamp = msg->header.stamp;
+
+  cv::Mat img(static_cast<int>(_imageScaling*cvImgPtr->image.rows),
+              static_cast<int>(_imageScaling*cvImgPtr->image.cols),
+              cvImgPtr->image.type());
+
+  if ( _imageScaling == 1.0 )
+    cvImgPtr->image.copyTo(img);
+  else
+  {
+    cv::resize(cvImgPtr->image, img, img.size());
+  }
+
+  std::vector<cv::Rect> detections;
+
+  // Perform detection
+  detectPersons(img, detections);
+
+  if ( _imageScaling != 1.0 )
+  {
+    scaleDetections(detections,
+                    static_cast<double>(cvImgPtr->image.cols)/static_cast<double>(img.cols),
+                    static_cast<double>(cvImgPtr->image.rows)/static_cast<double>(img.rows));
+  }
+
+  publishDetections(detections);
+}
+
+void PersonDetector::scaleDetections(std::vector<cv::Rect>& detections,
+                                     double scaleX, double scaleY) const
+{
+  BOOST_FOREACH(cv::Rect& detection, detections)
+  {
+    cv::Rect roi(detection);
+    detection.x      = static_cast<long>(roi.x      * scaleX);
+    detection.y      = static_cast<long>(roi.y      * scaleY);
+    detection.width  = static_cast<long>(roi.width  * scaleX);
+    detection.height = static_cast<long>(roi.height * scaleY);
+  }
+}
+
+
+void PersonDetector::detectPersons(const cv::Mat& img,
+                                   std::vector<cv::Rect>& detections)
+{ 
+  double start = static_cast<double>(cv::getTickCount());
+
+  _hogCPU->detectMultiScale(img,
+                            detections,
+                            0,                //hit threshold: decrease in order to increase number of detections but also false alarms
+                            cv::Size(8,8),    //win stride
+                            cv::Size(0,0),    //padding 24,16
+                            1.02,             //scaling
+                            1,                //final threshold
+                            false);            //use mean-shift to fuse detections
+
+  double stop = static_cast<double>(cv::getTickCount());
+  ROS_DEBUG_STREAM("Elapsed time in detectMultiScale: " << 1000.0*(stop-start)/cv::getTickFrequency() << " ms");
+}
+
+void PersonDetector::publishDetections(const std::vector<cv::Rect>& detections) const
+{
+  object_recognition_msgs::RecognizedObject msg;
+
+  // Create message for detected human
+  BOOST_FOREACH(const cv::Rect& roi, detections)
+  {
+    msg.header.frame_id = "";
+    msg.header.stamp    = _imgTimeStamp;
+
+    msg.type.key = "human";
+  }
+
+  _detectionPub.publish(msg);
+}
